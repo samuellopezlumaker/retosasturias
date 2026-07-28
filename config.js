f75d0359f8a8673095961b21401fea1485f8e7b0
samuellopezlumaker <samuellopezlu@gmail.com>
2026-07-28 13:18:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@
 // 3. Copia la "Project URL" y la "anon public key" aquí abajo
 // 4. Ejecuta el SQL de "supabase_setup.sql" en el SQL Editor de Supabase
 
-const SUPABASE_URL = "https://tu-proyecto.supabase.co";
+const SUPABASE_URL = "https://zmeekoljoaixdsmpnnqy.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_BFFFWRNNjZxFaNQ7JnBlCA_-evYQBN4";
